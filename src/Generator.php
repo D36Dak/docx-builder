2eb5826ed1aeb5baf1de0d014069bf9e004dc00f
Ignas Rupeika <ignas.rupeika@meritstory.com>
2026-05-09 17:42:52 +0300

    Initial structure

---
+++ src/Generator.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace D36Dak\DocxFromTemplates;
+
+class Generator
+{
+    public function getStats(): array
+    {
+        return [
+            'working' => true,
+            'created_at' => new \DateTimeImmutable('2026-05-18'),
+            'updated_at' => new \DateTimeImmutable('now'),
+        ];
+    }
+}
